2a86114bcc16c1348734e64fe886e10466f6f1dd
Adam Britt <adam@adambritt.com>
2019-08-02 00:22:47 +0200

    first commit

---
+++ Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 200
+const ACCELERATION = 50
+const JUMP_HEIGHT = -550
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	var friction = false
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
+	else:
+		friction = true
+		$Sprite.play("Idle")
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(
+			motion.x, 0, 0.2)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
+		
+	
+	motion = move_and_slide(motion, UP)
